e75865536b11b06fc92b14eed300f612ef2bff73
Xin Meng <mengxin.city@gmail.com>
2017-02-02 17:54:12 +0000

    capsule bestSort out of client

--- src/main/java/io/github/xmeng1/AbstractSortService.java
+++ src/main/java/io/github/xmeng1/AbstractSortService.java
@@ -1,5 +1,9 @@
 package io.github.xmeng1;
 
+import org.reflections.Reflections;
+
+import java.util.Set;
+
 /**
  * User:    mengxin
  * Date:    02/02/17
@@ -13,5 +17,33 @@ public abstract class AbstractSortService implements SortService{
 
     public int getTime() {
         return 0;
+    }
+
+    public static String bestSort() throws IllegalAccessException, InstantiationException {
+        System.out.println("Start to find the best strategies for sort!\n");
+        Reflections reflections = new Reflections("io.github.xmeng1");
+        int lowestTime=100000;
+        Class<? extends AbstractSortService> bestStrategy = null;
+        Set<Class<? extends AbstractSortService>> subTypes = reflections.getSubTypesOf(AbstractSortService.class);
+        int size = subTypes.size();
+        System.out.println("There are " + size + " strategies to compare!\n");
+        int i=0;
+        for (Class<? extends AbstractSortService> subType : subTypes) {
+            System.out.println("Check the " + (i+1) + " strategy!\n");
+            AbstractSortService strategy = subType.newInstance();
+            if (strategy.getTime() <= lowestTime) {
+                System.out.println("The strategy time of " + strategy.getClass().getSimpleName() +
+                        " is lower than current lowest time.\n");
+                bestStrategy = subType;
+                lowestTime = strategy.getTime();
+            }else{
+                System.out.println("The strategy time of " + strategy.getClass().getSimpleName() +
+                        " is higher than current lowest time.\n");
+            }
+            i++;
+        }
+        assert bestStrategy != null;
+        AbstractSortService strategy = bestStrategy.newInstance();
+        return strategy.sort();
     }
 }
--- src/main/java/io/github/xmeng1/Main.java
+++ src/main/java/io/github/xmeng1/Main.java
@@ -1,37 +1,9 @@
 package io.github.xmeng1;
 
-import org.reflections.Reflections;
-
-import java.util.Set;
-
 public class Main {
 
     public static void main(String[] args) throws IllegalAccessException, InstantiationException {
-        System.out.println("Start to find the best strategies for sort!\n");
-        Reflections reflections = new Reflections("io.github.xmeng1");
-        int lowestTime=100000;
-        Class<? extends AbstractSortService> bestStrategy = null;
-        Set<Class<? extends AbstractSortService>> subTypes = reflections.getSubTypesOf(AbstractSortService.class);
-        int size = subTypes.size();
-        System.out.println("There are " + size + " strategies to compare!\n");
-        int i=0;
-        for (Class<? extends AbstractSortService> subType : subTypes) {
-            System.out.println("Check the " + (i+1) + " strategy!\n");
-            AbstractSortService strategy = subType.newInstance();
-            if (strategy.getTime() <= lowestTime) {
-                System.out.println("The strategy time of " + strategy.getClass().getSimpleName() +
-                        " is lower than current lowest time.\n");
-                bestStrategy = subType;
-                lowestTime = strategy.getTime();
-            }else{
-                System.out.println("The strategy time of " + strategy.getClass().getSimpleName() +
-                        " is higher than current lowest time.\n");
-            }
-            i++;
-        }
-        assert bestStrategy != null;
-        AbstractSortService strategy = bestStrategy.newInstance();
 
-        System.out.println("Best sort strategy: " + strategy.sort() + "\n");
+        System.out.println("Best sort strategy: " + AbstractSortService.bestSort() + "\n");
     }
 }
